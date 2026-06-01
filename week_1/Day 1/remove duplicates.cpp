203d84b44fc1d3b941c5344986efad1b49135f98
Raunak11-tandon <raunaktandon11@gmail.com>
2026-06-01 22:46:39 +0530

    Create remove duplicates.cpp

---
+++ week_1/Day 1/remove duplicates.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // int arr[]
+        int n=nums.size();
+        int i=0;
+        for(int j=i+1;j<n;j++)
+        {  
+            if (nums[i]!=nums[j])
+            { 
+                nums[i+1]=nums[j];
+                 i++;
+            }
+        
+
+
+        }
+        return (i+1);
+    }
+};
