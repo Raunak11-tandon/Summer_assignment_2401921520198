140ae6b575bda3df833f09478af6e3f1695aec0e
Raunak11-tandon <raunaktandon11@gmail.com>
2026-06-02 22:00:02 +0530

    Create duplicate.cpp

---
+++ week_1/Day 2/duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size(); 
+        map<int,int>mpp; 
+        for(int i=0;i<n;i++){ 
+            mpp[nums[i]]+=1;
+        }     
+        for(auto it:mpp) 
+        {
+            if( it.second >1 ) 
+            return true;
+        }
+     return false ;
+    }       
+
+        
+    
+};
